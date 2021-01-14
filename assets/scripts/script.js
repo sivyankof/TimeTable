d685f737f099d6bdccc6d85686aabf2728d039b3
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-01-14 21:22:31 +0300

    проект1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,30 @@
-/*let clickOn = document.querySelector ('button [data-action="show"]');
-document.addEventListener ('çlick', function(event) {
-    let button = event.target;
-    let actionType = button.dataset.action;
+document.addEventListener('click', function(event) {
+    let id = event.target.dataset.toggleId;
+    if (!id) return;
+
+    let elem = document.getElementById(id);
+    if (!elem) return;
+    elem.hidden = !elem.hidden
+
+    //event.target.classList.add('red');
+     });
+
+
+
+/*let showList = document.querySelector ('button [data-action="hide"]').hidden = true; {
+    let showList = document.querySelector ('button [data-action="show"]').hidden = false;
+  }*/
+  
+  
+/*document.addEventListener('click', function(event) {
+    let hideList = document.querySelector ('button [data-action="hide"]');
+    document.getElementById('list').hidden = true;
+
+    let showlist = document.querySelector ('button [data-action="show"]')
+    showlist.hidden = true;
 }*/
 
 
+/*document.querySelector('button [data-action="hide"]').addEventListener("click", function() {
+    document.getElementById("list").hidden = true;
+  });*/
