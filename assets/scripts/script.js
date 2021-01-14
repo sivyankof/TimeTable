a46f83c0ab3f5fb017756630a930b9b455c3d177
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-01-14 19:44:38 +0300

    проект1

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+/*let clickOn = document.querySelector ('button [data-action="show"]');
+document.addEventListener ('çlick', function(event) {
+    let button = event.target;
+    let actionType = button.dataset.action;
+}*/
+
+
