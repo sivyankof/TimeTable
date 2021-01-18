47668ca90a2cfe384da079da0461560af4fa6532
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-01-18 16:41:03 +0300

    firts part

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,30 +1,40 @@
+let show_button = document.querySelector('button[data-action="show"]');
+let hide_button = document.querySelector('button[data-action="hide"]');
+let list_container = document.querySelector('.list-container');
+let checkboxes = document.querySelectorAll("ul input[type='checkbox']:checked");
+
 document.addEventListener('click', function(event) {
-    let id = event.target.dataset.toggleId;
-    if (!id) return;
+  let action = event.target.dataset.action;
+  if (!action) return;
 
-    let elem = document.getElementById(id);
-    if (!elem) return;
-    elem.hidden = !elem.hidden
+  this.show = function() {
+      console.log('show function');
+      show_button.classList.remove('active');
+      hide_button.classList.add('active');
+      list_container.classList.remove('hide');
+  }
 
-    //event.target.classList.add('red');
-     });
+  this.hide = function() {
+    console.log('hide function');
+    hide_button.classList.remove('active');
+    show_button.classList.add('active');
+    list_container.classList.add('hide');
+  }
+  
 
-
-
-/*let showList = document.querySelector ('button [data-action="hide"]').hidden = true; {
-    let showList = document.querySelector ('button [data-action="show"]').hidden = false;
+ /* let numberOfCheckedItems = 0;  
+    for(var i = 0; i < checkboxes.length; i++)  
+    {  
+        if(checkboxes[i].checked)  
+            numberOfCheckedItems++;  
+    }  
+    if(checkboxes.checked)  
+    { checkboxes.classList.add('hide') 
+    } else {
+      checkboxes.classList.add('show')
   }*/
-  
-  
-/*document.addEventListener('click', function(event) {
-    let hideList = document.querySelector ('button [data-action="hide"]');
-    document.getElementById('list').hidden = true;
 
-    let showlist = document.querySelector ('button [data-action="show"]')
-    showlist.hidden = true;
-}*/
-
-
-/*document.querySelector('button [data-action="hide"]').addEventListener("click", function() {
-    document.getElementById("list").hidden = true;
-  });*/
+  if(event.target.classList.contains('active')) {
+    this[action]();
+  }
+ });
